dce680ab2390049205e03a7eac8ba4685c160ca6
aasimd <dongarkar21@gmail.com>
2023-06-05 02:04:55 +0530

    added target blank to footer links

--- src/Components/FooterCard/FooterCard.jsx
+++ src/Components/FooterCard/FooterCard.jsx
@@ -10,24 +10,39 @@ export const FooterCard = () => {
 			<div>
 				<h2>
 					<i class="fa-regular fa-copyright"></i>Developed by{" "}
-					<a href="https://aasim-portfolio.netlify.app/">Aasim</a>
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href="https://aasim-portfolio.netlify.app/"
+					>
+						Aasim
+					</a>
 				</h2>
 			</div>
 			<div>
 				<ul>
 					<li>
-						<a target="_blank" rel="noreferrer" href="https://github.com/aasimd">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://github.com/aasimd"
+						>
 							<i class="fa-brands fa-github"></i>
 						</a>
 					</li>
 					<li>
-						<a target="_blank" rel="noreferrer" href="https://twitter.com/dthree01">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://twitter.com/dthree01"
+						>
 							<i class="fa-brands fa-square-twitter"></i>
 						</a>
 					</li>
 					<li>
 						<a
-							target="_blank" rel="noreferrer"
+							target="_blank"
+							rel="noreferrer"
 							href="https://linkedin.com/in/aasim-dongarkar-28b365231"
 						>
 							<i class="fa-brands fa-linkedin"></i>
