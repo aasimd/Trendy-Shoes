fd494a628a55cf12aa2a05274f37a9b0faf71eeb
aasimd <dongarkar21@gmail.com>
2023-06-07 07:09:58 +0530

    added responsive ui to landing page

--- src/Components/ServicesCard/ServicesCard.jsx
+++ src/Components/ServicesCard/ServicesCard.jsx
@@ -9,22 +9,27 @@ export const ServicesCard = () => {
 				<ul>
 					<li>
 						<p>
-							<i className="fa-solid fa-truck"></i> <b>Free Shipping</b>
+							<i className="fa-solid fa-truck"></i> <br />
+							<b>Free Shipping</b>
 						</p>
 					</li>
 					<li>
 						<p>
-							<i className="fa-solid fa-medal"></i> <b>Quality Products</b>
+							<i className="fa-solid fa-medal"></i> <br />
+							<b>Quality Products</b>
 						</p>
 					</li>
 					<li>
 						<p>
-							<i className="fa-sharp fa-solid fa-box"></i> <b>14 days return</b>
+							<i className="fa-sharp fa-solid fa-box"></i>
+							<br />
+							<b>14 days return</b>
 						</p>
 					</li>
 					<li>
 						<p>
-							<i class="fa-sharp fa-solid fa-headphones"></i>{" "}
+							<i class="fa-sharp fa-solid fa-headphones"></i>
+							<br />
 							<b>24/7 Support</b>
 						</p>
 					</li>
