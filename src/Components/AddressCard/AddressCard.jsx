610074c61645441467fdd00a5456e47cae963e62
aasimd <dongarkar21@gmail.com>
2023-07-17 17:57:43 +0530

    added responsive ui to checkout and profile page

--- src/Components/AddressCard/AddressCard.jsx
+++ src/Components/AddressCard/AddressCard.jsx
@@ -1,191 +1,199 @@
 /** @format */
 
-import React, { useState } from "react";
-import { useContext } from "react";
-import { PageContext } from "../../contexts/PageContext";
+import React, { useState } from 'react'
+import { useContext } from 'react'
+import { PageContext } from '../../contexts/PageContext'
 
 export const AddressCard = ({ address }) => {
-	const { id, name, city, addressState, pincode, street } = address;
-	const { dispatch, state } = useContext(PageContext);
-	const [editAddress, setEditAddress] = useState(false);
+    const { id, name, city, addressState, pincode, street } = address
+    const { dispatch, state } = useContext(PageContext)
+    const [editAddress, setEditAddress] = useState(false)
 
-	const prevAddress = {
-		id: id,
-		name: name,
-		street: street,
-		pincode: pincode,
-		city: city,
-		addressState: addressState
-	};
-	const [newAddress, setNewAddress] = useState({
-		id: prevAddress.id,
-		name: prevAddress.name,
-		street: prevAddress.street,
-		pincode: prevAddress.pincode,
-		city: prevAddress.city,
-		addressState: prevAddress.addressState
-	});
-	const testFunction = (id) => {
-		console.log(id);
-		console.log(state.addressData);
-	};
-	const editSaveChangesHandler = (id) => {
-		setEditAddress(false);
-		const newAddressArray = state.addressData.map((address) =>
-			address.id === id
-				? {
-						...address,
-						name: newAddress.name,
-						street: newAddress.street,
-						pincode: newAddress.pincode,
-						city: newAddress.city,
-						addressState: newAddress.addressState
-				  }
-				: address
-		);
-		dispatch({ type: "setDeliveryAddress", payload: {} });
-		dispatch({ type: "addNewAddress", payload: newAddressArray });
-	};
-	return (
-		<li>
-			{editAddress ? (
-				<div className="address-edit-container">
-					<div>
-						<label>Name: </label>
-						<input
-							type="text"
-							placeholder={`${name}`}
-							onChange={(event) => {
-								if (event.target.value.length > 0) {
-									setNewAddress((prev) => ({
-										...prev,
-										name: event.target.value
-									}));
-								} else {
-									setNewAddress((prev) => ({
-										...prev,
-										name: prevAddress.name
-									}));
-								}
-							}}
-						/>
-					</div>
-					<div>
-						<label>Street: </label>
-						<input
-							type="text"
-							placeholder={`${street}`}
-							onChange={(event) => {
-								if (event.target.value.length > 0) {
-									setNewAddress((prev) => ({
-										...prev,
-										street: event.target.value
-									}));
-								} else {
-									setNewAddress((prev) => ({
-										...prev,
-										street: prevAddress.street
-									}));
-								}
-							}}
-						/>
-					</div>
+    const prevAddress = {
+        id: id,
+        name: name,
+        street: street,
+        pincode: pincode,
+        city: city,
+        addressState: addressState,
+    }
+    const [newAddress, setNewAddress] = useState({
+        id: prevAddress.id,
+        name: prevAddress.name,
+        street: prevAddress.street,
+        pincode: prevAddress.pincode,
+        city: prevAddress.city,
+        addressState: prevAddress.addressState,
+    })
+    const testFunction = (id) => {
+        console.log(id)
+        console.log(state.addressData)
+    }
+    const editSaveChangesHandler = (id) => {
+        setEditAddress(false)
+        const newAddressArray = state.addressData.map((address) =>
+            address.id === id
+                ? {
+                      ...address,
+                      name: newAddress.name,
+                      street: newAddress.street,
+                      pincode: newAddress.pincode,
+                      city: newAddress.city,
+                      addressState: newAddress.addressState,
+                  }
+                : address
+        )
+        dispatch({ type: 'setDeliveryAddress', payload: {} })
+        dispatch({ type: 'addNewAddress', payload: newAddressArray })
+    }
+    return (
+        <li>
+            {editAddress ? (
+                <div className="address-edit-container">
+                    <div>
+                        <label>Name: </label>
+                        <input
+                            type="text"
+                            placeholder={`${name}`}
+                            onChange={(event) => {
+                                if (event.target.value.length > 0) {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        name: event.target.value,
+                                    }))
+                                } else {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        name: prevAddress.name,
+                                    }))
+                                }
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <label>Street: </label>
+                        <input
+                            type="text"
+                            placeholder={`${street}`}
+                            onChange={(event) => {
+                                if (event.target.value.length > 0) {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        street: event.target.value,
+                                    }))
+                                } else {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        street: prevAddress.street,
+                                    }))
+                                }
+                            }}
+                        />
+                    </div>
 
-					<div>
-						<label>Pincode: </label>
-						<input
-							type="number"
-							placeholder={`${pincode}`}
-							onChange={(event) => {
-								if (event.target.value.length > 0) {
-									setNewAddress((prev) => ({
-										...prev,
-										pincode: event.target.value
-									}));
-								} else {
-									setNewAddress((prev) => ({
-										...prev,
-										pincode: prevAddress.pincode
-									}));
-								}
-							}}
-						/>{" "}
-					</div>
-					<div>
-						<label>City: </label>
-						<input
-							type="text"
-							placeholder={`${city}`}
-							onChange={(event) => {
-								if (event.target.value.length > 0) {
-									setNewAddress((prev) => ({
-										...prev,
-										city: event.target.value
-									}));
-								} else {
-									setNewAddress((prev) => ({
-										...prev,
-										city: prevAddress.city
-									}));
-								}
-							}}
-						/>
-					</div>
-					<div>
-						<label>State: </label>
-						<input
-							type="text"
-							placeholder={`${addressState}`}
-							onChange={(event) => {
-								if (event.target.value.length > 0) {
-									setNewAddress((prev) => ({
-										...prev,
-										addressState: event.target.value
-									}));
-								} else {
-									setNewAddress((prev) => ({
-										...prev,
-										addressState: prevAddress.addressState
-									}));
-								}
-							}}
-						/>
-					</div>
-					<button
-						onClick={() => {
-							editSaveChangesHandler(id);
-							setEditAddress(false);
-						}}
-					>
-						Save Changes
-					</button>
-				</div>
-			) : (
-				<div className="address-container">
-					<h2>{newAddress.name}</h2>
-					<p>
-						{newAddress.street}
-						<br />
-						{newAddress.pincode}
-						<br />
-						{newAddress.city}, {newAddress.addressState}
-					</p>
-					<div>
-						<button onClick={() => setEditAddress(true)}>Edit</button>
-						<button
-							onClick={() => {
-								const newarray = state.addressData.filter(
-									(address) => address.id !== id
-								);
-								dispatch({ type: "setDeleteAddress", payload: newarray });
-								dispatch({ type: "setDeliveryAddress", payload: {} });
-							}}
-						>
-							Delete
-						</button>
-					</div>
-				</div>
-			)}
-		</li>
-	);
-};
+                    <div>
+                        <label>Pincode: </label>
+                        <input
+                            type="number"
+                            placeholder={`${pincode}`}
+                            onChange={(event) => {
+                                if (event.target.value.length > 0) {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        pincode: event.target.value,
+                                    }))
+                                } else {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        pincode: prevAddress.pincode,
+                                    }))
+                                }
+                            }}
+                        />{' '}
+                    </div>
+                    <div>
+                        <label>City: </label>
+                        <input
+                            type="text"
+                            placeholder={`${city}`}
+                            onChange={(event) => {
+                                if (event.target.value.length > 0) {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        city: event.target.value,
+                                    }))
+                                } else {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        city: prevAddress.city,
+                                    }))
+                                }
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <label>State: </label>
+                        <input
+                            type="text"
+                            placeholder={`${addressState}`}
+                            onChange={(event) => {
+                                if (event.target.value.length > 0) {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        addressState: event.target.value,
+                                    }))
+                                } else {
+                                    setNewAddress((prev) => ({
+                                        ...prev,
+                                        addressState: prevAddress.addressState,
+                                    }))
+                                }
+                            }}
+                        />
+                    </div>
+                    <button
+                        onClick={() => {
+                            editSaveChangesHandler(id)
+                            setEditAddress(false)
+                        }}
+                    >
+                        Save Changes
+                    </button>
+                </div>
+            ) : (
+                <div className="address-container">
+                    <h2>{newAddress.name}</h2>
+                    <p>
+                        {newAddress.street}
+                        <br />
+                        {newAddress.pincode}
+                        <br />
+                        {newAddress.city}, {newAddress.addressState}
+                    </p>
+                    <div className="edit-delete-address-btn">
+                        <button onClick={() => setEditAddress(true)}>
+                            Edit
+                        </button>
+                        <button
+                            onClick={() => {
+                                const newarray = state.addressData.filter(
+                                    (address) => address.id !== id
+                                )
+                                dispatch({
+                                    type: 'setDeleteAddress',
+                                    payload: newarray,
+                                })
+                                dispatch({
+                                    type: 'setDeliveryAddress',
+                                    payload: {},
+                                })
+                            }}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            )}
+        </li>
+    )
+}
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,68 +1,71 @@
 /** @format */
-import "./ProfilePage.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import React, { useState, useContext, useEffect } from "react";
-import { PageContext } from "../../contexts/PageContext";
-import { NavBar } from "../../Components/NavBar/NavBar";
-import { AddressListCard } from "../../Components/AddressListCard/AddressListCard";
-import { AddNewAddressCard } from "../../Components/AddNewAddressCard/AddNewAddressCard";
-import { FooterCard } from "../../Components/FooterCard/FooterCard.jsx";
-import { ProgressBar } from "react-loader-spinner";
+import './ProfilePage.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import React, { useState, useContext, useEffect } from 'react'
+import { PageContext } from '../../contexts/PageContext'
+import { NavBar } from '../../Components/NavBar/NavBar'
+import { AddressListCard } from '../../Components/AddressListCard/AddressListCard'
+import { AddNewAddressCard } from '../../Components/AddNewAddressCard/AddNewAddressCard'
+import { FooterCard } from '../../Components/FooterCard/FooterCard.jsx'
+import { ProgressBar } from 'react-loader-spinner'
 export const ProfilePage = () => {
-	const { state, dispatch } = useContext(PageContext);
-	const [pageState, setPageState] = useState("ProfileInfo");
-	useEffect(() => {
-		dispatch({ type: "changeIsLoading", payload: true });
-		setTimeout(() => {
-			dispatch({ type: "changeIsLoading", payload: false });
-		}, 500);
-	}, []);
-	const logoutHandler = () => {
-		dispatch({ type: "setLogin", payload: false });
-	};
-	return (
-		<div>
-			<div
-				className={state.isLoading ? "loader-spinner" : "loader-spinner-hidden"}
-			>
-				<ProgressBar
-					height="100px"
-					width="400px"
-					ariaLabel="progress-bar-loading"
-					wrapperStyle={{}}
-					wrapperClass="progress-bar-wrapper"
-					borderColor="#F4442E"
-					barColor="#51E5FF"
-				/>
-			</div>
-			<ToastContainer />
-			<div className="profile-page">
-				<nav className="profile-nav">
-					<NavBar />
-				</nav>
-				<div className="profile-buttons">
-					<button
-						onClick={() => setPageState("ProfileInfo")}
-						className={
-							pageState === "ProfileInfo"
-								? "active-profile-button"
-								: "inactive-profile-button"
-						}
-					>
-						Profile Information
-					</button>
-					<button
-						onClick={() => setPageState("Addresses")}
-						className={
-							pageState === "Addresses"
-								? "active-profile-button"
-								: "inactive-profile-button"
-						}
-					>
-						Addresses
-					</button>
-					{/* <button
+    const { state, dispatch } = useContext(PageContext)
+    const [pageState, setPageState] = useState('ProfileInfo')
+    useEffect(() => {
+        dispatch({ type: 'changeIsLoading', payload: true })
+        setTimeout(() => {
+            dispatch({ type: 'changeIsLoading', payload: false })
+        }, 500)
+    }, [])
+    const logoutHandler = () => {
+        dispatch({ type: 'setLogin', payload: false })
+    }
+    return (
+        <div>
+            <div
+                className={
+                    state.isLoading ? 'loader-spinner' : 'loader-spinner-hidden'
+                }
+            >
+                <ProgressBar
+                    height="100px"
+                    width="400px"
+                    ariaLabel="progress-bar-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="progress-bar-wrapper"
+                    borderColor="#F4442E"
+                    barColor="#51E5FF"
+                />
+            </div>
+            <ToastContainer />
+
+            <nav className="profile-nav">
+                <NavBar />
+            </nav>
+            <div className="profile-page">
+                <div className="profile-buttons">
+                    <button
+                        onClick={() => setPageState('ProfileInfo')}
+                        className={
+                            pageState === 'ProfileInfo'
+                                ? 'active-profile-button'
+                                : 'inactive-profile-button'
+                        }
+                    >
+                        Profile Information
+                    </button>
+                    <button
+                        onClick={() => setPageState('Addresses')}
+                        className={
+                            pageState === 'Addresses'
+                                ? 'active-profile-button'
+                                : 'inactive-profile-button'
+                        }
+                    >
+                        Addresses
+                    </button>
+                    {/* <button
 						onClick={() => setPageState("OrderHistory")}
 						className={
 							pageState === "OrderHistory"
@@ -72,41 +75,43 @@ export const ProfilePage = () => {
 					>
 						Order History
 					</button> */}
-				</div>
-				<div className="profile-content">
-					{pageState === "Addresses" && (
-						<div>
-							<div className="address-section">
-								<div>
-									<AddressListCard />
-								</div>
-								<div>
-									<AddNewAddressCard />
-								</div>
-							</div>
-						</div>
-					)}
-					{pageState === "ProfileInfo" && (
-						<section>
-							<div className="user-profile-section">
-								<h1>User Profile</h1>
-								<p>
-									<span>Name </span>
-									{`${state.userInfo.firstName} ${state.userInfo.lastName}`}
-								</p>
-								<p>
-									<span>Email </span>
-									{state.userInfo.email}
-								</p>
-								<button onClick={() => logoutHandler()}>Logout</button>
-							</div>
-						</section>
-					)}
-				</div>
-			</div>
-			<footer>
-				<FooterCard />
-			</footer>
-		</div>
-	);
-};
+                </div>
+                <div className="profile-content">
+                    {pageState === 'Addresses' && (
+                        <div>
+                            <div className="address-section">
+                                <div>
+                                    <AddressListCard />
+                                </div>
+                                <div>
+                                    <AddNewAddressCard />
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    {pageState === 'ProfileInfo' && (
+                        <section>
+                            <div className="user-profile-section">
+                                <h1>User Profile</h1>
+                                <p>
+                                    <span>Name </span>
+                                    {`${state.userInfo.firstName} ${state.userInfo.lastName}`}
+                                </p>
+                                <p>
+                                    <span>Email </span>
+                                    {state.userInfo.email}
+                                </p>
+                                <button onClick={() => logoutHandler()}>
+                                    Logout
+                                </button>
+                            </div>
+                        </section>
+                    )}
+                </div>
+            </div>
+            <footer>
+                <FooterCard />
+            </footer>
+        </div>
+    )
+}
